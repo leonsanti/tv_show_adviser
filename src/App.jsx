91d391a36561cec3ec4b32cac8d9deb7588a6530
Santiago Leon <santiago.leon@fundingcircle.com>
2023-07-22 21:46:50 -0300

    Fetch popular TVShow

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
-export function App(){
-  return <div>Hello World!</div>;
+import { useState, useEffect } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import { BACKDROP_BASE_URL } from "./config";
+import style from "./style.module.css";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const popularTVShowList = await TVShowAPI.fetchPopulars();
+    console.log(popularTVShowList);
+    if (popularTVShowList.length > 0) {
+      setCurrentTVShow(popularTVShowList[0]);
+    }
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  console.log(currentTVShow);
+  return (
+    <div
+      className={style.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+           url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={style.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={style.tv_show_detail}>Tv show detail</div>
+      <div className={style.recommended_tv_shows}>Recommended to you</div>
+    </div>
+  );
 }
