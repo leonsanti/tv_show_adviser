edd3cb2b2ed5750d8e283f5780a1f7e3bf2672a9
Santiago Leon <santiago.leon@fundingcircle.com>
2023-08-02 19:28:35 -0300

    Move api key to .env

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -4,21 +4,21 @@ import { FAKE_POPULARS, FAKE_RECOMMENDATIONS } from "./fake_data";
 
 export class TVShowAPI {
   static async fetchPopulars() {
-    const url = `${BASE_URL}tv/popular${API_KEY_PARAM}`;
+    const url = `${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}`;
     const response = await axios.get(url);
     return response.data.results;
     // return FAKE_POPULARS;
   }
 
   static async fetchRecommendations(tvShowId) {
-    const url = `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`;
+    const url = `${BASE_URL}tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}`;
     const response = await axios.get(url);
     return response.data.results;
     // return FAKE_RECOMMENDATIONS;
   }
 
   static async fetchByTitle(title) {
-    const url = `${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`;
+    const url = `${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${title}`;
     const response = await axios.get(url);
     return response.data.results;
     // return FAKE_RECOMMENDATIONS;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=1cbfe474a06399217e73b64cd07d45c6";
+const API_KEY_PARAM = "1cbfe474a06399217e73b64cd07d45c6";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
